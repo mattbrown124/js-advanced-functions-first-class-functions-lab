64d78aa7bfde646c4407b07decd7dc87697d6f45
Matt Brown <matthewtbrown124@gmail.com>
2021-04-08 13:40:47 -0500

    commit tree 0c27ce67a6a81d5b7410eebe9badf47eb10f085b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const drivers = ["Sally", "Bob", "Freddy", "Claudia"]
+function returnFirstTwoDrivers() {
+    return drivers.slice(0,2)
+}
+function returnLastTwoDrivers() {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(someInt) {
+    return fare => someInt * fare
+}
+
+function fareDoubler(int) {
+    return 2 * int
+}
+
+function fareTripler(int) {
+    return 3 * int
+}
+
+function selectDifferentDrivers(drivers, select) {
+    if(select === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers()
+    } else if(select === returnLastTwoDrivers) {
+        return returnLastTwoDrivers()
+    }
+}
